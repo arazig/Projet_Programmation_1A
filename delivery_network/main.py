215b4a32c9dade1aeee8b47ec38768172e8cb59d
arazig <amine.razig@ensae.fr>
2023-03-09 21:14:17 +0000

    added a program to calculate the time needed to extract the min power for all trips

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,51 +1,42 @@
-from graph import Graph, graph_from_file
-
+from graph import Graph, graph_from_file, graph_from_file_bis
+import time
+from random import randint
 
 data_path = "input/"
-file_name = "network.02.in"
+file_name = "network.01.in"
 
-g = graph_from_file(data_path + file_name)
+g = graph_from_file_bis(data_path + file_name)
 print(g)
-
-assert ( frozenset({1,2,3}) == frozenset({3,2,1}))
 
 for comp in Graph.connected_components_set(g):
     print(comp)
 
+#Graph.get_path_with_power(g, 6, 5, 100)
 
-def get_path_with_power(g, src, dest, power):
-        #raise NotImplementedError
 
-        """Cette fonction permet de svoir si il existe un trajet possible entre 
-        deux neouds pour un camion avec une puissance donnée. Si oui, elle nous renvoie un 
-        des chemins possible.
+# Estimation du temps necessaire pour calculer la puissance minimale ( et le chemin associé) sur l'ensemble
+# des trajets pour chacuns des fichier routes.x.in
+def mean(L=[]):
+    s=0
+    for n in range(0,len(L)):
+        s += n
+    return s/len(L)
 
-        Args:
-            src (int): source, starting node
-            dest (int): destinantion
-            power (int): power of the truck
-        """
-        #Tout d'abord, on regarde si les deux noeuds sont la meme composantes, si non on retourne None
-        #samecomp= False
-        #for comp in Graph.connected_components_set(g):
-        #    if (frozenset([src,dest])) in comp :
-        #        samecomp = True
-       # if samecomp == False :
-         #   return None
 
-        #A present il nous faut récuperer tous les chemins possibles entre les deux noeuds
-        explo=[(src,[src])]
-        list_paths = [] # list of the paths between the nodes
-        while explo : 
-            node, path = explo.pop(0) # cela nous permet d'actualiser les chemins explo
-            n_neigh = [g.graph[node][j] for j in range(0,len(g.graph[node]))] #list of (node's neighbours,powermin,dist)
-            for neighb in n_neigh : 
-                powermin = neighb[1]
-                if neighb[0] not in path and power >= powermin:  #for each neighbor accessible (by truck's power) not yet in the path
-                    if neighb[0] == dest:
-                        list_paths.append(path + [neighb[0]])
-                    else:
-                        explo.append((neighb[0], path + [neighb[0]]))
-        return None if list_paths ==[] else list_paths
+Time=[]
+for _ in range(0,10):
 
-print(get_path_with_power(g, 1, 4,3))
+    src = randint(1, g.nb_nodes)
+    dest = randint(1, g.nb_nodes) 
+
+    t0= time.perf_counter()
+    Graph.min_power(g, src, dest)
+    t1=time.perf_counter()
+
+    t= t1 - t0
+    print("Trajet : "+str(src)+ " ----> " + str(dest)+ "\n t = "+ str(t))
+    Time.append(t)
+
+print("Le temps moyen d'execution de min_power est : "+str(mean(Time)))
+nbr_trajets = 1
+print("Approximation du temps necessaire pour l'ensemble des trajets est : "+ str(mean(Time)*nbr_trajets))
