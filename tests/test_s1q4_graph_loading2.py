00b69a2e144adadc95b50db63495a37599d810c5
arazig <amine.razig@ensae.fr>
2023-03-09 14:03:12 +0000

    added graph_from_file modified test

--- tests/test_s1q4_graph_loading2.py
+++ tests/test_s1q4_graph_loading2.py
@@ -1,1 +1,23 @@
-# À compléter
+#a completer
+
+import sys 
+sys.path.append("delivery_network/")
+
+import unittest 
+from graph import Graph, graph_from_file
+
+class Test_GraphLoading(unittest.TestCase):
+    def test_network04(self):
+        g = graph_from_file("input/network.04.in")
+        self.assertEqual(g.nb_nodes, 10)
+        self.assertEqual(g.nb_edges, 4)
+        self.assertEqual(g.graph[1][0][2], 6)
+        self.assertEqual(g.graph[3][1][2], 2)
+
+    def test_network1(self):
+        g = graph_from_file("input/network.01.in")
+        self.assertEqual(g.nb_nodes, 7)
+        self.assertEqual(g.nb_edges, 5)
+
+if __name__ == '__main__':
+    unittest.main()
