5df981ddfc16ef1636dae9e268aceeea40794f6a
arazig <razigamine@gmail.com>
2023-04-01 15:54:59 +0200

    added trucks_selection function to to recover an approximate solution of the problem

--- delivery_network/Opti_profit.py
+++ delivery_network/Opti_profit.py
@@ -19,57 +19,89 @@ sys.setrecursionlimit(5000000)
 
 
 #  Création des fichier de puissance minimales associées a chaque trajet
+# ( étape effectuée au préalable dans le programme powermin_file )
 
 
-def power_min_file(x):
+
+###### selction d'un ensemble de camion qui approche une sollution optimal du problème
+
+def trucks_selection(x, Budget = 25*10e9) : 
     """
-    creation a power file in the format power.x.in associated of a routes.x.in
-    params :
-        x (int) : index of each file
+    Cette fonction permet d'effecteur une selection approché de camion qui va permetre de maximiser 
+    l'utilité sous contrainte de budget. On se concentre sur le réseau d'indice x. 
+    args :
+        x(int)        > indice du réseau
+        Budger(float) > budget disponible
+    output :
+        Selection(list)  > liste de la selection de camion proposée
     """
+    # recupération des fichiers 
+    route_file = 'input/routes.' + str(x) + '.in'
+    powermin_file = 'input/powermin.' + str(x) + '.in'
+    trucks_file = 'input/trucks.' + str(x) + '.in'
 
-    directory_path_in = "/Users/aminerazig/Desktop/ENSAE 1A/INFO/Projet_Programmation_1A/input/"
-    data_path = "input/"
+    Selection = [] 
+    # Ouverture des fichier et stockage des valeurs dans des listes
+    Trajets = []
+    with open(route_file, "r") as route :
+        with open(powermin_file, "r") as power :
+            n = int(route.readline())
+            for _ in range(n):
 
-    g = graph_from_file(data_path + 'network.'+str(x)+'.in')
-    or_tree = oriented_tree_construction(kruskal(g))
+                src, dest, utility = map(int, route.readline().split())
+                pwr = int(power.readline())
+                Trajets.append((src, dest, utility, pwr))
 
-    route_file = data_path + 'routes.' + str(x) + '.in'
-    powermin_file = data_path + 'powermin.' + str(x) + '.in'
+    Trucks = []      
+    with open(trucks_file, "r") as trucks :   
+        nb_trucks = trucks.readline()
+        for _ in range(int(nb_trucks)) :
+            pow, cost = map(int, trucks.readline().split())
+            Trucks.append((pow, cost))
 
-    #  Ouverture du fichier 
-    with open(powermin_file , "w") as out :
+    #  tri de l'ensemble de trajets par utilité decroissante           
+    Trajets = sorted(Trajets, key=lambda utility:utility[2], reverse = True)
 
-        with open(route_file, "r") as file :
-            n = file.readline().split()
-            n = int(n[0])            #  numbers of trips
-            lines =file.readlines()  #  c'est une liste de lignes ["1 2 3","2 3 5"]
-            out.write(str(n) + "\n") #  la première ligne du fichier se compose du nombre de trajets 
-            for line in lines[0:n-1] :
-                line = line.split()
+    #  tri du catalogue de camion par couts croissant  
+    Trucks = sorted(Trucks, key=lambda cost:cost[1])
 
-                if len(line)==3 :
-                    src, dest, weight = map(float,line) # Choice of parameters
-                    p = tree_min_power(or_tree, src, dest)[1]
-                
-                else :
-                        raise Exception("Incorrecte format")
-                out.write(str(p) + "\n")
-
-            # on ajoute manuellement la derniere ligne
-            line = lines[n-1].split()
-            if len(line)==3 :
-                    src, dest, weight = map(float,line) # Choice of parameters
-                    p = tree_min_power(or_tree, src, dest)[1]
-                
-            else :
-                    raise Exception("Incorrecte format")
-            out.write(str(p) )
-
-for i in range (1,11) :
-    power_min_file(i)
+    for trajet in Trajets :
+            for truck in Trucks :
+                if truck[0] >= trajet[3] and Budget - truck[1]>= 0:
+                    chx = truck
+                Budget = Budget - chx[1]
+                Selection.append(chx)
+            print("Budget épuisé. Voici une selection d'achat de camion")
+            return(Selection)
+    print("Budget épuisé. Voici une selection d'achat de camion")
+    return(Selection)
 
 
+print(trucks_selection(1))
+
+# REMARQUE IL EST TRES UTILE DE CREER UNE FONCTION QUI PERMET DE CALCULER L'utilité obtenue a partir d'une selelction de camion 
+# ou bien de directement la calculer ne emme temps que l'on donne la selection. 
+
+
+
+"""
+pseudo-code  explication : 
+on lis en simulatané les fichier route et power et on assoccie les valeusr des deux fichier dnas une liste de tuples L
+on trie la liste obtenue par utilité decroissante 
+on parcours les élemenst de cette liste 
+on trie le fichier trucks par couts décroissant
+tant que le budget est positif >0 :
+    on sélectionne le camion qui coute le moins chere avec une puissance adéquate (on parcours la liste trié et on prend le 1er qui est compatible)
+    condition : Truck[0] >= L[puissancemin]
+        Si  Budget >= Couts :
+            budget = budget - cout
+            trks_selection.append(truck)
+        else : 
+            print("budget épuisé, voici une selction d'achat de camion : ")
+            return trks_selection
+
+
+"""
 
 
 #################################################################
