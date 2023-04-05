83dcaf65ca68cebfdf0ab7e9e1ae39687298144c
arazig <razigamine@gmail.com>
2023-04-05 17:49:55 +0200

    added Optimal and approximate solution of utility optimisation

--- delivery_network/Approx_profit.py
+++ delivery_network/Approx_profit.py
@@ -1,8 +1,10 @@
 """
  L' approche théorique la plus instinctive est dans un premier temps de trier les trajets du fichiers
- route par profit décroissant. Ensuite on parcours chcuns des trajets en lui associant le camion qui coute le moins 
+ route par profit décroissant. Ensuite on parcours chacuns des trajets en lui associant le camion qui coute le moins 
  chere ayant la puissnace minimale requise. On effectue cette opération jusqu'a épuisement du budget ( ie somme des couts > B)
 
+Avantage : si le Budget est tres grand ca nous renvoie la sollution optimale puisque il nous suffit pour chaque trjet d'acheter
+le camion le moins chere sans épuiser notre budget
 """
 
 # Importation :
@@ -25,20 +27,21 @@ sys.setrecursionlimit(5000000)
 
 ###### selction d'un ensemble de camion qui approche une sollution optimal du problème
 
-def trucks_selection(x, Budget = 25*10e9) : 
+def trucks_selection(x, catalogue, Budget = 2000000000000000) : 
     """
     Cette fonction permet d'effecteur une selection approché de camion qui va permetre de maximiser 
     l'utilité sous contrainte de budget. On se concentre sur le réseau d'indice x. 
     args :
         x(int)        > indice du réseau
-        Budger(float) > budget disponible
+        catalogue(int > choix du catalogue de camion
+        Budget(float) > budget disponible
     output :
         Selection(list)  > liste de la selection de camion proposée
     """
     # recupération des fichiers 
     route_file = 'input/routes.' + str(x) + '.in'
     powermin_file = 'input/powermin.' + str(x) + '.in'
-    trucks_file = 'input/trucks.' + str(x) + '.in'
+    trucks_file = 'input/trucks.' + str(catalogue) + '.in'
 
     Selection = [] 
     # Ouverture des fichier et stockage des valeurs dans des listes
@@ -48,7 +51,7 @@ def trucks_selection(x, Budget = 25*10e9) :
             n = int(route.readline())
             for _ in range(n):
 
-                src, dest, utility = map(int, route.readline().split())
+                src, dest, utility = map(float, route.readline().split())
                 pwr = int(power.readline())
                 Trajets.append((src, dest, utility, pwr)) 
                 #  il est aussi possible de calculer directement pwr a chaque itération sans passer par lecture des fichier powermin
@@ -57,8 +60,8 @@ def trucks_selection(x, Budget = 25*10e9) :
     with open(trucks_file, "r") as trucks :   
         nb_trucks = trucks.readline()
         for _ in range(int(nb_trucks)) :
-            pow, cost = map(int, trucks.readline().split())
-            Trucks.append((pow, cost))
+            pow, cost = map(int, trucks.readline().split())      
+            Trucks.append((pow, cost)) 
 
     #  tri de l'ensemble de trajets par utilité decroissante           
     Trajets = sorted(Trajets, key=lambda utility:utility[2], reverse = True)
@@ -66,22 +69,47 @@ def trucks_selection(x, Budget = 25*10e9) :
     #  tri du catalogue de camion par couts croissant  
     Trucks = sorted(Trucks, key=lambda cost:cost[1])
 
-    for trajet in Trajets :
-            for truck in Trucks :
-                if truck[0] >= trajet[3] and Budget - truck[1]>= 0:
-                    chx = truck
-                Budget = Budget - chx[1]
-                Selection.append(chx)
-            print("Budget épuisé. Voici une selection d'achat de camion")
-            return(Selection)
-    print("Budget épuisé. Voici une selection d'achat de camion")
-    return(Selection)
+    for trajet in Trajets:
+        for truck in Trucks:
+            if truck[0] >= trajet[3] and Budget >= truck[1]:
+                Selection.append(truck)
+                Budget -= truck[1]
+                break
+        if Budget < truck[1]:  # Sortie de la boucle si le budget est épuisé
+            break
 
+    return Selection
 
-print(trucks_selection(1))
+t0 = time.perf_counter()
+print(trucks_selection(10,2))
+t1 = time.perf_counter()
+t= t1 - t0
+print (str(10)+ ": " + str(t) )
 
+"""
+for x in range (1,11):
+    t0 = time.perf_counter()
+    print(trucks_selection(x,2))
+    t1 = time.perf_counter()
+    t= t1 - t0
+    print (str(x)+ ": " + str(t) )
+"""
 # REMARQUE IL EST TRES UTILE DE CREER UNE FONCTION QUI PERMET DE CALCULER L'utilité obtenue a partir d'une selelction de camion 
 # ou bien de directement la calculer ne emme temps que l'on donne la selection. 
+
+
+"""
+1: 0.02570429899969895
+2: 1.131312251000054
+3: 129.39552251000168
+4: 3.3461553619999904
+5: 0.7697823119997338
+6: 159.58119179999994
+7: 152.4702746389994
+8: 125.82133894599974
+9: 44.99317726300069
+"""
+
 
 
 
@@ -101,48 +129,4 @@ tant que le budget est positif >0 :
             print("budget épuisé, voici une selction d'achat de camion : ")
             return trks_selection
 
-
 """
-
-
-############################################################################
-#######  2nd METHODE : Une sollution optimale du problème  (dynamique)  ####
-############################################################################
-
-# Tout d'abord, on recupere l'utilité max que peut generer chaque camion du catalogue
-x=1
-# recupération des fichiers 
-route_file = 'input/routes.' + str(x) + '.in'
-powermin_file = 'input/powermin.' + str(x) + '.in'
-trucks_file = 'input/trucks.' + str(x) + '.in'
-
-Selection = [] 
-# Ouverture des fichier et stockage des valeurs dans des listes
-Trajets = []
-with open(route_file, "r") as route :
-    with open(powermin_file, "r") as power :
-        n = int(route.readline())
-        for _ in range(n):
-
-            src, dest, utility = map(int, route.readline().split())
-            pwr = int(power.readline())
-            Trajets.append((src, dest, utility, pwr)) 
-            #  il est aussi possible de calculer directement pwr a chaque itération sans passer par lecture des fichier powermin
-
-Trucks = []      
-with open(trucks_file, "r") as trucks :   
-    nb_trucks = trucks.readline()
-    for _ in range(int(nb_trucks)) :
-        pow, cost = map(int, trucks.readline().split())
-        umax = 0
-        for trj in Trajets :
-            # si la puissance du c&mion est suffisante et que l'utilité est meilleure on actualise l'utilité max
-            if pow >= trj[3] and umax < trj[2] : 
-                umax = trj[2]
-        # on associe à chaque camion l'utilité max qu'il peut rapporter         
-        Trucks.append((pow, cost, umax))
-
-print(Trucks)
-        
-        
-
---
+++ delivery_network/Optimal_profit.py
@@ -0,0 +1,125 @@
+
+"""
+ L' approche théorique la plus instinctive est dans un premier temps de trier les trajets du fichiers
+ route par profit décroissant. Ensuite on parcours chcuns des trajets en lui associant le camion qui coute le moins 
+ chere ayant la puissnace minimale requise. On effectue cette opération jusqu'a épuisement du budget ( ie somme des couts > B)
+
+"""
+
+# Importation :
+from graph import Graph, graph_from_file, kruskal, oriented_tree_construction, tree_min_power
+import time
+from random import randint
+import sys
+sys.setrecursionlimit(5000000)
+
+
+
+############################################################################
+#######  2nd METHODE : Une sollution optimale du problème  (dynamique)  ####
+############################################################################
+
+# Tout d'abord, on recupere l'utilité max que peut generer chaque camion du catalogue
+
+def Truck(x, catalogue):
+    """
+    Cette fonction nous permet d'associer a chaque camion du catalogue l'utilité maximale 
+    qu'il peur engendrer sur l'intégralité des trajets d'un fichier route 
+
+    args : 
+        x(int): indice du fichier routes.x.in (de 1 à 10)
+        catalogue(int): indice du catalogue de camion ( 0, 1 ou 2)
+    output :
+        Trucks(list) : liste de tous les camions du catalogue avec l'utilité maximal que chacun engendre
+    """
+    # recupération des fichiers 
+    route_file = 'input/routes.' + str(x) + '.in'
+    powermin_file = 'input/powermin.' + str(x) + '.in'
+    trucks_file = 'input/trucks.' + str(catalogue) + '.in'
+
+    Selection = [] 
+    # Ouverture des fichier et stockage des valeurs dans des listes
+    Trajets = []
+    with open(route_file, "r") as route :
+        with open(powermin_file, "r") as power :
+            n = int(route.readline())
+            for _ in range(n):
+
+                src, dest, utility = map(int, route.readline().split())
+                pwr = int(power.readline())
+                Trajets.append((src, dest, utility, pwr)) 
+                #  il est aussi possible de calculer directement pwr a chaque itération sans passer par lecture des fichier powermin
+
+    Trucks = []      
+    with open(trucks_file, "r") as trucks :   
+        nb_trucks = trucks.readline()
+        for _ in range(int(nb_trucks)) :
+            pow, cost = map(int, trucks.readline().split())
+            umax = 0
+            for trj in Trajets :
+                # si la puissance du c&mion est suffisante et que l'utilité est meilleure on actualise l'utilité max
+                if pow >= trj[3] and umax < trj[2] : 
+                    umax = trj[2]
+            # on associe à chaque camion l'utilité max qu'il peut rapporter         
+            Trucks.append((pow, cost, umax)) 
+    return Trucks
+
+print(Truck(6,1))
+
+
+
+def opti(x, catalogue, Budget):
+    '''
+    On utilise un algorithme de type sac à dos où la capacité du sac est donnée par le budget B et les éléments à ranger 
+    dans le sac à dos sont les camions en fonction de leurs prix.
+    
+    Cette fonction permet d'effecteur une selection optimale de camion qui va permetre de maximiser 
+    l'utilité sous contrainte de budget. On se concentre sur le réseau d'indice x. 
+    args :
+        x(int)        > indice du réseau
+        Budget(float) > budget disponible
+        Catalogue(int)> indice du catalogue de camion ( 0, 1 ou 2)
+    output :
+        Selection(list)  > liste de la selection de camion proposée
+    '''
+    Trucks = Truck(x,catalogue)
+    matrice = [[0 for x in range (int(Budget + 1))] for x in range(int(len(Trucks)+1))] 
+    # on ajoute des deux cotés 1 pour pouvoir représenter le cas où le budget est nul et celui où il n'y a aucun camion sélectionné
+
+    for i in range(1, len(Trucks) + 1): 
+        # on prend l'indice d'un camion
+        for w in range(1, Budget + 1): 
+            # on va regarder pour chaque discretisation de notre budget
+            if Trucks[i-1][1] <= w:  
+                # si le prix du camion est inférieur au budget à cette étape on peut ajouter le camion au catalogue
+                matrice[i][w] = max(Trucks[i-1][2] + matrice[i-1][w-Trucks[i-1][1]], matrice[i-1][w])
+                # on prend le max entre l'utilité de la ligne prédédente de la matrice pour le meme budget et l'utilité
+                # du camion choisi auquel on ajoute la solution optimisé pour le budget correspondant au bugdet actuel moins le prix du camion
+            else:
+                # sinon quand on ne peut pas acheter le camion on se contente de la sélection faite précedemment pour le budget courant 
+                matrice[i][w] = matrice[i-1][w]
+
+    # on va maintenant remonter la matrice afin de trouver la solution optimale du problème i.e la selection optimale de camions ainsi que la flotte de camions sélectionnées
+    n = len(Trucks)
+    elements_selection = []
+
+    while Budget >= 0 and n >= 0:
+        # on parcours la matrice tant que le budget n'est pas épuisé ou tant que les camions sélectionnés n'ont pas tous été 
+        # parcouru en commençant par le dernier élément de la matrice (en bas à droite) correspondant à la sélection optimale
+        e = Trucks[n-1]
+        if Budget-e[1] >= 0 : 
+            if matrice[n][Budget] == matrice[n-1][Budget-e[1]] + e[2]:
+                # on regarde si l'utilité courante ne serait pas la somme des utilités 
+                # du camion sélectionné et de celle obtenu quand le budget est la différence entre
+                # le budget actuel et le prix du camion 
+                elements_selection.append(e)
+                Budget -= e[1]
+
+            n -= 1
+        else : 
+            utilite_max = matrice[-1][-1]
+            return utilite_max, elements_selection
+
+
+print(opti(1,1,2500000))
+
