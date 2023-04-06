cdf90d039901b9606a08078ebb268e69cbe5957b
arazig <amine.razig@ensae.fr>
2023-04-06 13:30:24 +0000

    modif

--- delivery_network/Approx_profit.py
+++ delivery_network/Approx_profit.py
@@ -126,92 +126,11 @@ def Truck_utility(x, camions):
     return sum(t[2] for t in Trucks)
 
 #  EXEMPLE : ------------------------------------------------------------------------
-#  selection du fichier routes : 
-route_file = 1
-#  on récupère une sélection de camions
-Liste_camion = trucks_selection(route_file, 1, 25*10e9)
-print(Liste_camion)
-#  On affiche l'utilité que cette selection rapporte : 1399580
-print(Truck_utility(route_file, Liste_camion))
-
-
-# REMARQUE : Il est assez utile de créer une fonction qui calcule l'utilité obtenue a partir d'une selelction de camion 
-# afin de pouvoir effectuer des comparaisons avce d'autres méthode de résolution du problème. 
-"""
-def max_utility(L,x):
-    
-    Cette fonction nous permet de connaitre l'utilité qu'engendre uen sélection de camion sur un trajet donné.
-    args : 
-        L (list) : c'est la liste des camions sélectionnés au préalable
-        x (int) : indice du fichier route
-    return :
-        umax(int or float) : l'utilité généré par notre selection pour un fichier route
-    
-
-    route_file = 'input/routes.' + str(x) + '.in'
-    powermin_file = 'input/powermin.' + str(x) + '.in'
-    u_opt = 0
-    for camion in L : 
-        umax = 0
-        with open(route_file, "r") as route, open(powermin_file, "r") as power: 
-            n = int(route.readline())
-            for _ in range(n):
-                # on récupère l'utilite et la puissance minimale de chaque trajet
-                src, dest, utility = map(float, route.readline().split())
-                pwr = int(power.readline())
-                # si la puissance du c&mion est suffisante et que l'utilité est meilleure on actualise l'utilité max
-                if camion[0] >= pwr and umax < utility : 
-                    umax = utility
-        u_opt += umax
-    return u_opt
-
-    
-print(max_utility(trucks_selection(2,2),2))
-"""
-
-
-#  LES TEMPS DE CALCULS de la fonction trucks_selection en fonction des différents fichier routes
-
-""" Output :
-for x in range (1,11):
-    t0 = time.perf_counter()
-    print(trucks_selection(x,2))
-    t1 = time.perf_counter()
-    t= t1 - t0
-    print (str(x)+ ": " + str(t) )
-
-
-1: 0.02570429899969895
-2: 1.131312251000054
-3: 129.39552251000168
-4: 3.3461553619999904
-5: 0.7697823119997338
-6: 159.58119179999994
-7: 152.4702746389994
-8: 125.82133894599974
-9: 44.99317726300069
-10: 153.22190753999894
-
-"""
-
-
-
-
-
-"""
-pseudo-code  explication : 
-on lis en simulatané les fichier route et power et on assoccie les valeusr des deux fichier dnas une liste de tuples L
-on trie la liste obtenue par utilité decroissante 
-on parcours les élemenst de cette liste 
-on trie le fichier trucks par couts décroissant
-tant que le budget est positif >0 :
-    on sélectionne le camion qui coute le moins chere avec une puissance adéquate (on parcours la liste trié et on prend le 1er qui est compatible)
-    condition : Truck[0] >= L[puissancemin]
-        Si  Budget >= Couts :
-            budget = budget - cout
-            trks_selection.append(truck)
-        else : 
-            print("budget épuisé, voici une selction d'achat de camion : ")
-            return trks_selection
-
-"""
+# selection du fichier routes : 
+#route_file = 1
+# On récupère une sélection de camions
+#Liste_camion = trucks_selection(route_file, 2)
+#print(Liste_camion)
+# On affiche l'utilité que cette selection rapporte :
+#print(Truck_utility(route_file, Liste_camion))
+#-------------------------------------------------------------------------------------
